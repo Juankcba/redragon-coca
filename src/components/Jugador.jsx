7c285604218667bdbcdb019fdca09a438878b07c
Juan Carlos Gonzalez <juangblade@hotmail.com>
2021-12-21 20:58:43 -0300

    cambio

--- src/components/Jugador.jsx
+++ src/components/Jugador.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 function Jugador({ jugador }) {
   return (
-    <div className="mt-5 flex justify-between  bg-white shadow-md rounded-lg py-10 px-5 w-full">
-      <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {""}</p>
-      <span>{jugador.name}</span>
-      <p className="font-bold mb-3 text-gray-700 uppercase">Juego: {""}</p>
-      <span>{jugador.game}</span>
+    <div className="mt-5   bg-white shadow-md rounded-lg py-10 px-5 w-full">
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Nombre: {""} <span className="font-normal">{jugador.name}</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Juego: {""}
+        <span className="font-normal">{jugador.game}</span>
+      </p>
     </div>
   );
 }
--- src/components/ListadoJugadores.jsx
+++ src/components/ListadoJugadores.jsx
@@ -3,13 +3,13 @@ import Jugador from "./Jugador";
 
 const ListadoJugadores = ({ jugadores }) => {
   return (
-    <div className="md:w-1/2 ">
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen ">
       {jugadores.length > 0 ? (
         <h2 className="text-center text-white font-bold uppercase">
-          Proximos jugadores
+          Jugadores Participantes
         </h2>
       ) : null}
-      <div className="md:h-screen overflow-y-auto no-scrollbar w-full mx-auto">
+      <div className="md:h-screen overflow-y-auto no-scrollbar w-10/12 mx-auto">
         {jugadores.map((jugador) => (
           <Jugador jugador={jugador} />
         ))}
