bc0764b0354d4580e41ca33abf5b7b8d225d07e4
Juan Carlos Gonzalez <juangblade@hotmail.com>
2021-12-21 21:38:27 -0300

    cambios

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,8 @@
+import React from "react";
 import latas from "../../assets/img/latas.svg";
-const Footer = () => {
+function Footer() {
+  let year = new Date();
+
   return (
     <footer className=" h-24 w-full text-center text-white">
       <img
@@ -8,10 +11,12 @@ const Footer = () => {
         className="p-0 m-0  w-full h-20 mb--10 bg-repeat-x position-relative-bottom"
       />
       <div className="bg-black ">
-        <h3 className=" py-8">Todos los derechos reservados</h3>
+        <h3 className=" py-8">
+          &copy; Todos los derechos reservados {year.getFullYear()}
+        </h3>
       </div>
     </footer>
   );
-};
+}
 
 export default Footer;
