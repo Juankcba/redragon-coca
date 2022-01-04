cc30ab4c426df832b81fe4d3306c0a267e2305b6
juankcba <juangblade@hotmail.com>
2022-01-04 19:11:23 -0300

    cambios

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,38 +1,15 @@
 import React, { useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { Outlet } from "react-router-dom";
+
 import { Link } from "react-router-dom";
-const Layout = ({ usuarioAutenticado }) => {
-  const [visible, setVisible] = useState(false);
+const Layout = ({ children }) => {
+  console.log(children);
   return (
     <div className="container-fluid mx-auto ">
-      {visible && (
-        <div className=" z-10 absolute top-0 left-0 w-full h-screen   ">
-          <div className="bg-slate-500 opacity-50 w-full h-screen  relative z-0 top-0 left-0"></div>
-          <div className="bg-white w-2/3 md:w-3/12 h-screen p-5 opacity-100 absolute top-0 left-0 z-10 mx-auto">
-            <div className="flex justify-between">
-              <h2 className="text-center font-bold text-lg ">Menu </h2>
-              <button onClick={() => setVisible(!visible)}>Cerrar</button>
-            </div>
-            <nav className="flex flex-col" onClick={() => setVisible(false)}>
-              <Link to="/registro-jugadores">Registro Jugadores</Link>
-              <Link to="/registro-ganadores">Registro Ganadores</Link>
-              <Link to="/jugadores">Jugadores</Link>
-              {usuarioAutenticado && (
-                <>
-                  <Link to="/registro">Sorteo</Link>
-                  <Link to="/jugadores/nuevo">Agregar Jugadores</Link>
-                  <Link to="/registro/nuevo">Agregar Participante</Link>
-                </>
-              )}
-            </nav>
-          </div>
-        </div>
-      )}
-      <Header setVisible={setVisible} />
+      <Header setVisible={false} />
       <section className="contenido-minimo overflow-y-scroll">
-        <Outlet />
+        {children}
       </section>
 
       <Footer />
