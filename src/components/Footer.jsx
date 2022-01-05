52fdbd2b8722a8444ada049782a579ea0a9cc49f
juankcba <juangblade@hotmail.com>
2022-01-05 09:02:37 -0300

    cambios en los logos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,18 +8,10 @@ function Footer() {
   return (
     <footer className="h-max  w-full text-center text-white">
       <div className="bg-black xl:px-20 pt-4 flex xl:flex-row flex-col justify-end  gap-8">
-        <img
-          src={logo}
-          alt="logo-coca-cola"
-          className=" h-12 w-auto object-contain cursor-pointer"
-        />
-        <img
-          src={logored}
-          alt="logo-redragon"
-          className="w-auto object-contain h-16 "
-        />
+        <img src={logo} alt="logo-coca-cola" className="logo-coca-footer" />
+        <img src={logored} alt="logo-redragon" className="logo-footer " />
 
-        <h3 className=" py-8 text-center align-middle">
+        <h3 className="py-8 text-center align-middle">
           &copy; Blade Link - Todos los derechos reservados {year.getFullYear()}
         </h3>
       </div>
