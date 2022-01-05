2e9c2ec89bba773af14e8f806a7dfd4d87b13fa3
juankcba <juangblade@hotmail.com>
2022-01-05 09:07:35 -0300

    cambio en logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,19 +4,15 @@ import logored from "../../assets/img/redragon-logo.png";
 function Header({ setVisible }) {
   return (
     <div className="bg-white h-32 ">
-      <div className="flex justify-between w-10/12 mx-auto pt-10  pb-10 h-28">
+      <div className="flex justify-between w-10/12 mx-auto pt-4  pb-4 h-28">
         <img
           src={logo}
           alt="logo-coca-cola"
-          className=" h-12 w-auto object-contain cursor-pointer"
+          className="logo-header"
           onClick={() => setVisible(true)}
         />
 
-        <img
-          src={logored}
-          alt="logo-redragon"
-          className="w-auto object-contain h-16 "
-        />
+        <img src={logored} alt="logo-redragon" className="logo-footer" />
       </div>
       <div className="divisor-header w-full"></div>
     </div>
